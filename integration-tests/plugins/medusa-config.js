0cf4cb7456074c00c99e2ab7bd413f69d277575f
zka-c <111402300+zka-c@users.noreply.github.com>
2023-01-24 16:47:38 +0800

    Update medusa-config.js

--- integration-tests/plugins/medusa-config.js
+++ integration-tests/plugins/medusa-config.js
@@ -1,8 +1,13 @@
-const DB_HOST = process.env.DB_HOST
+const DB_NAME = process.env.DB_TEMP_NAME
 const DB_USERNAME = process.env.DB_USERNAME
 const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_NAME = process.env.DB_TEMP_NAME
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_DATABASE = process.env.DB_DATABASE
 
+const DATABASE_URL = 
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}` + 
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
 module.exports = {
   plugins: [
     {
@@ -27,5 +32,6 @@ module.exports = {
     database_type: "postgres",
     jwt_secret: "test",
     cookie_secret: "test",
+    database_extra: { ssl: { rejectUnauthorized: false } },
   },
 }
